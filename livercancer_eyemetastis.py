2a6a9930c8c6838e7a5521a7c610235ceaeaa019
Learning <88524372+shimunana@users.noreply.github.com>
2022-12-03 14:48:36 +0800

    Update livercancer_eyemetastis.py

--- livercancer_eyemetastis.py
+++ livercancer_eyemetastis.py
@@ -3,22 +3,26 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import sklearn
-from sklearn.neural_network import MLPClassifier
-from sklearn.impute import SimpleImputer
+# from sklearn.neural_network import MLPClassifier
+# from sklearn.impute import SimpleImputer
 from imblearn.over_sampling import SMOTE
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.linear_model import LogisticRegression
-from xgboost import XGBClassifier
+# from sklearn.ensemble import RandomForestRegressor
+# from xgboost import XGBClassifier
+import seaborn as sns
+import shap
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 #应用标题
+#%%不提示warning信息
+st.set_option('deprecation.showPyplotGlobalUse', False)
 st.set_page_config(page_title='Prediction model for Ocular metastasis of hepatocellular carcinoma')
 st.title('Ocular metastasis from primary liver cancer: Machine Learning-Based development and interpretation study')
 st.sidebar.markdown('## Variables')
-AFP_400 = st.sidebar.selectbox('AFP_400',('≤400','>400'),index=1)
-CEA = st.sidebar.slider("CEA", 0.00, 400.00, value=7.68, step=0.01)
-CA125 = st.sidebar.slider("CA125", 0.00, 5000.00, value=3320.00, step=0.01)
-CA199 = st.sidebar.slider("CA199", 0.00, 2000.00, value=59.61, step=0.01)
-ALP = st.sidebar.slider("ALP", 0, 2000, value=215, step=1)
-TG = st.sidebar.slider("TG", 0.00,10.00, value=1.42, step=0.01)
+AFP_400 = st.sidebar.selectbox('AFP_400(μg/L)',('≤400','>400'),index=1)
+CEA = st.sidebar.slider("CEA(μg/L)", 0.00, 120.00, value=7.68, step=0.01)
+CA125 = st.sidebar.slider("CA125(μg/L)", 0.00, 500.00, value=30.00, step=0.01)
+CA199 = st.sidebar.slider("CA199(μg/L)", 0.00, 500.00, value=59.61, step=0.01)
+ALP = st.sidebar.slider("ALP(U/L)", 0, 1000, value=215, step=1)
+TG = st.sidebar.slider("TG(mmol/L)", 0.00,10.00, value=1.42, step=0.01)
 
 #分割符号
 st.sidebar.markdown('#  ')
@@ -29,24 +33,17 @@ st.sidebar.markdown('##### For communication and cooperation, please contact wsh
 map = {'≤400':0,'>400':1}
 AFP_400 =map[AFP_400]
 # 数据读取，特征标注
+#%%load model
+import joblib
+xgb_model = joblib.load('gbm_model_liver_eye.pkl')
 hp_train = pd.read_csv('github_data.csv')
-hp_train['M'] = hp_train['M'].apply(lambda x : +1 if x==1 else 0)
 features =["AFP_400","CEA","CA125","CA199",'ALP','TG']
 target = 'M'
-random_state_new = 50
-data = hp_train[features]
-X_data = data
-X_ros = np.array(X_data)
-y_ros = np.array(hp_train[target])
-oversample = SMOTE(random_state = random_state_new)
-X_ros, y_ros = oversample.fit_resample(X_ros, y_ros)
-#XGB_model = XGBClassifier(n_estimators=360, max_depth=2, learning_rate=0.1,random_state = random_state_new)
-XGB_model = LogisticRegression(penalty="none", random_state=random_state_new)
-XGB_model.fit(X_ros, y_ros)
+y = np.array(hp_train[target])
 sp = 0.5
 #figure
-is_t = (XGB_model.predict_proba(np.array([[AFP_400,CEA,CA125,CA199,ALP,TG]]))[0][1])> sp
-prob = (XGB_model.predict_proba(np.array([[AFP_400,CEA,CA125,CA199,ALP,TG]]))[0][1])*1000//1/10
+is_t = (xgb_model.predict_proba(np.array([[AFP_400,CEA,CA125,CA199,ALP,TG]]))[0][1])> sp
+prob = (xgb_model.predict_proba(np.array([[AFP_400,CEA,CA125,CA199,ALP,TG]]))[0][1])*1000//1/10
 
 
 if is_t:
@@ -58,3 +55,54 @@ if st.button('Predict'):
     if result == '  Low Risk Ocular metastasis':
         st.balloons()
     st.markdown('## Probability of High Risk Ocular metastasis group:  '+str(prob)+'%')
+
+    #%%SHAP
+    col_names = features
+    X_last = pd.DataFrame(np.array([[AFP_400,CEA,CA125,CA199,ALP,TG]]))
+    X_last.columns = col_names
+    X_raw = hp_train[features]
+    X = pd.concat([X_raw,X_last],ignore_index=True)
+    if is_t:
+        y_last = 1
+    else:
+        y_last = 0
+    
+    y_raw = (np.array(hp_train[target]))
+    y = np.append(y_raw,y_last)
+    y = pd.DataFrame(y)
+        
+    
+    model = xgb_model
+    #%%
+    sns.set()
+    explainer = shap.Explainer(model, X)
+    shap_values = explainer.shap_values(X)
+    a = len(X)-1
+    #%%SHAP Force logit plot
+    # st.subheader('SHAP Force logit plot')
+    # fig, ax = plt.subplots(figsize=(12, 6), dpi=300)
+    # force_plot = shap.force_plot(explainer.expected_value,
+    #                 shap_values[a, :], 
+    #                 X.iloc[a, :], 
+    #                 figsize=(25, 3),
+    #                 # link = "logit",
+    #                 matplotlib=True,
+    #                 out_names = "Output value")
+    # st.pyplot(force_plot)
+    #%%SHAP Water PLOT
+    st.subheader('SHAP Water plot')
+    shap_values = explainer(X) # 传入特征矩阵X，计算SHAP值
+    fig, ax = plt.subplots(figsize=(8, 8), dpi=300)
+    waterfall_plot = shap.plots.waterfall(shap_values[a,:])
+    st.pyplot(waterfall_plot)
+    #%%ConfusionMatrix
+    st.subheader('Confusion Matrix of XGB')
+    xgb_prob = xgb_model.predict(X)
+    cm = confusion_matrix(y, xgb_prob)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=['NOM', 'OM'])
+    sns.set_style("white")
+    disp.plot(cmap='RdPu')
+    plt.title("Confusion Matrix of XGB")
+    disp1 = plt.show()
+    st.pyplot(disp1)
+
